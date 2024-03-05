62e874bca395875f78fb64452927443ebe9f5823
Ruan lana <lanaruanpablo@gmail.com>
2024-03-04 22:01:18 -0300

    Atualizacao do Projeto

--- OrientacaoObjeto/src/br/com/orientacaoObjeto/model/Funcionario.java
+++ OrientacaoObjeto/src/br/com/orientacaoObjeto/model/Funcionario.java
@@ -1,5 +1,38 @@
 package br.com.orientacaoObjeto.model;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
+	
+	int matricula;
+	String setor;
+	
+	// IMPLEMENTAR CONSTRUTORES
+	public Funcionario() {
+		
+	}
+	
+	// GET AND SET
+	
+	public Funcionario(int matricula, String setor) {
+		this.matricula = matricula;
+		this.setor = setor;
+	}
 
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	
+	// ENVIAR PARA O GIT 
+	
 }
